499c0c9b2e1b76199d264317d6e4486c350f8ad2
Miguel Pieters <miguelpieters@mbp-van-miguel.home>
2020-08-03 13:17:45 +0200

    Add functionality to extract quizzes and enrollment

--- IguideME.Web/Services/CanvasTest.cs
+++ IguideME.Web/Services/CanvasTest.cs
@@ -1,8 +1,20 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using GraphQL;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Web;
+using UvA.Connectors.Canvas.Helpers;
 using UvA.DataNose.Connectors.Canvas;
 
 namespace IguideME.Web.Services
@@ -10,13 +22,22 @@ namespace IguideME.Web.Services
     public class CanvasTest
     {
         CanvasApiConnector connector;
+        public string baseUrl;
+        public string accessToken;
+        public string apiVersioning = "api/v1/";
 
         public CanvasTest(IConfiguration config)
         {
+            this.accessToken = config["Canvas:AccessToken"];
+            this.baseUrl = config["Canvas:Url"];
             connector = new CanvasApiConnector(config["Canvas:Url"], config["Canvas:AccessToken"]);
         }
 
         public string[] GetStudents(int courseId)
             => connector.FindCourseById(courseId).GetUsersByType(EnrollmentType.Student).Select(s => s.Name).ToArray();
+
+        public List<Quiz> GetQuizzes(int courseId) {
+            return connector.FindCourseById(courseId).Quizzes;
+        }
     }
 }
