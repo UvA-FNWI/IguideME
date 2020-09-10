349e2c94fee572d71269fbcd314c39ce4faf4dee
miguelpieters <64592997+miguelpieters@users.noreply.github.com>
2020-09-10 23:47:47 +0200

    Add Canvas connector

--- IguideME.Web/Services/CanvasTest.cs
+++ IguideME.Web/Services/CanvasTest.cs
@@ -1,20 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using GraphQL;
-using GraphQL.Client.Http;
-using GraphQL.Client.Serializer.Newtonsoft;
-using Newtonsoft.Json.Linq;
-using System.IO;
-using System.Net;
 using System.Net.Http;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Web;
-using UvA.Connectors.Canvas.Helpers;
 using UvA.DataNose.Connectors.Canvas;
 
 namespace IguideME.Web.Services
@@ -25,6 +12,7 @@ namespace IguideME.Web.Services
         public string baseUrl;
         public string accessToken;
         public string apiVersioning = "api/v1/";
+        private static readonly HttpClient client = new HttpClient();
 
         public CanvasTest(IConfiguration config)
         {
@@ -36,8 +24,65 @@ namespace IguideME.Web.Services
         public string[] GetStudents(int courseId)
             => connector.FindCourseById(courseId).GetUsersByType(EnrollmentType.Student).Select(s => s.Name).ToArray();
 
-        public List<Quiz> GetQuizzes(int courseId) {
-            return connector.FindCourseById(courseId).Quizzes;
+        public Submission[] GetSubmissions(int courseID, int userID)
+        {
+            // Returns all submissions of type 'on_paper' for a specified user.
+            Submission[] submissions = connector
+                .FindCourseById(courseID)
+                .Assignments.Where(a => a.SubmissionTypes.ToList().Contains(SubmissionType.OnPaper))
+                .Select(a => a.Submissions.Where(s => s.UserID == userID.ToString()))
+                .SelectMany(i => i).ToArray();
+
+            return submissions;
+        }
+
+        public float[] GetAllSubmissionGrades(int courseID)
+        {
+            // Returns all submissions of type 'on_paper' for a specified user.
+            float[] grades = connector
+                .FindCourseById(courseID)
+                .Assignments.Where(a => a.SubmissionTypes.ToList().Contains(SubmissionType.OnPaper))
+                .Select(a => a.Submissions).SelectMany(i => i).Select(x => float.Parse(x.Grade)).ToArray();
+
+            return grades;
+        }
+
+        public Dictionary<string, object> GetQuizzes(int courseID, int userID) {
+            /*
+             * Returns all quizzes available to a specified user.
+             * 
+             * Response format:
+             * {
+             *   "submissions": [List of submissions],
+             *   "quizzes": [Lis of quizzes],
+             *   "questions": [User answer per question]
+             * }
+             */
+
+            IEnumerable<Assignment> assignments = connector
+                .FindCourseById(courseID)
+                .Assignments.Where(a => a.SubmissionTypes.ToList().Contains(SubmissionType.Quiz));
+
+            // fetch all quiz submissions for specified user
+            Submission[] submissions = assignments
+                .Select(a => a.Submissions.Where(s => s.UserID == userID.ToString()))
+                .SelectMany(i => i).ToArray();
+
+            // fetch course quizzes
+            Quiz[] quizzes = connector.FindCourseById(courseID).Quizzes.ToArray();
+
+            return new Dictionary<string, object>
+            {
+                { "peer_comp",  assignments.Select(a => a.Submissions.Where(x => x.Grade != null).Select(x => float.Parse(x.Grade))).SelectMany(i => i)},
+                { "submissions", submissions },
+                { "quizzes", quizzes },
+                { "questions", quizzes.Select(quiz => quiz.Submissions.Select(submission => submission.Answers)).ToArray() }
+            };
+        }
+
+        public Discussion[] GetDiscussions(int courseId, string userName)
+        {
+            return connector.FindCourseById(courseId).Discussions.Where(d => d.UserName == userName).ToArray();
         }
     }
 }
