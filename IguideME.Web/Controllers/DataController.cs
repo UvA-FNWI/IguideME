85351d2caaf53375b2e26ff7d7a168883199016b
miguelpieters <64592997+miguelpieters@users.noreply.github.com>
2020-09-14 02:32:44 +0200

    cleanup for launch

--- IguideME.Web/Controllers/DataController.cs
+++ IguideME.Web/Controllers/DataController.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using IguideME.Web.Services;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System;
+using UvA.DataNose.Connectors.Canvas;
 
 namespace IguideME.Web.Controllers
 {
@@ -18,6 +20,7 @@ namespace IguideME.Web.Controllers
     {
         private readonly ILogger<DataController> logger;
         private readonly CanvasTest canvasTest;
+        private static readonly HttpClient client = new HttpClient();
 
         public DataController(ILogger<DataController> logger, CanvasTest canvasTest)
         {
@@ -35,6 +38,54 @@ namespace IguideME.Web.Controllers
         {
             var auth = await HttpContext.AuthenticateAsync();
             return auth.Principal.Identity.Name;
+        }
+
+        [HttpGet]
+        [Route("/Quizzes")]
+        public async Task<string> GetQuizzes()
+        {
+            var quizzes = canvasTest.GetQuizzes(994);
+            var submissions = quizzes.Select(quiz => quiz.Submissions);
+            var questions = new List<object>();
+
+            // fetch questions per quiz submission
+            for (int i = 0; i < submissions.Count(); i++)
+            {
+                var url = canvasTest.baseUrl + canvasTest.apiVersioning +
+                    $"courses/{quizzes[i].CourseID}/quizzes/{quizzes[i].ID}" +
+                    $"/questions?access_token={canvasTest.accessToken}";
+
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                // refrain from QuizSubmission type because the answers field is required
+                object[] quiz_submission_data = JsonConvert.DeserializeObject<object[]>(
+                    await response.Content.ReadAsStringAsync()
+                );
+
+                // append quiz questions to the collection
+                questions = questions.Concat(quiz_submission_data).ToList();
+            }
+
+            return JsonConvert.SerializeObject(questions);
+        }
+
+        [HttpGet]
+        [Route("/Enrollments")]
+        public async Task<string> GetEnrollments()
+        {
+            var url = canvasTest.baseUrl + canvasTest.apiVersioning +
+                    $"courses/994/enrollments?access_token={canvasTest.accessToken}";
+
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            // refrain from QuizSubmission type because the answers field is required
+            var enrollment = JsonConvert.DeserializeObject<Enrollment[]>(
+                await response.Content.ReadAsStringAsync()
+            ).Where(enrollment => enrollment.CourseID == 994).First();
+
+            return JsonConvert.SerializeObject(enrollment);
         }
     }
 }
