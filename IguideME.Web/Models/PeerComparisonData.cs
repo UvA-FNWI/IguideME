31ad3297076c8da6b984bb49e2efb02f90b79943
miguelpieters <64592997+miguelpieters@users.noreply.github.com>
2020-09-14 02:37:32 +0200

    Allow for easy grouping on the group_id key

---
+++ IguideME.Web/Models/PeerComparisonData.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System;
+
+namespace IguideME.Web.Models
+{
+    public class PeerComparisonData
+    {
+        public double Minimum { get; set; }
+        public double Maximum { get; set; }
+
+        public double Average { get; set; }
+
+        public PeerComparisonData(float[] grades)
+        {
+            this.Minimum = Math.Round(grades.Min(), 1);
+            this.Maximum = Math.Round(grades.Max(), 1);
+            this.Average = Math.Round(grades.Average(), 1);
+        }
+    }
+}
