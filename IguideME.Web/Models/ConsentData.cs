15b266cf2e745c9ef07189a8963b494be76f7147
miguelpieters <64592997+miguelpieters@users.noreply.github.com>
2020-09-15 00:57:32 +0200

    Include student name for exclusion on external data

--- IguideME.Web/Models/ConsentData.cs
+++ IguideME.Web/Models/ConsentData.cs
@@ -4,14 +4,16 @@
     {
         public int CourseID { get; set; }
         public int UserID { get; set; }
+        public string UserName { get; set; }
 
         // 1 = accepted, 0 = declined, -1 = unspecified
         public int Granted { get; set; }
 
-        public ConsentData(int CourseID, int UserID, int Granted)
+        public ConsentData(int CourseID, int UserID, string UserName, int Granted)
         {
             this.CourseID = CourseID;
             this.UserID = UserID;
+            this.UserName = UserName;
             this.Granted = Granted;
         }
     }
