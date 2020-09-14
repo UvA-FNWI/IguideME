ec0eccb769438bd0967f602ebde46339ceb444fa
miguelpieters <64592997+miguelpieters@users.noreply.github.com>
2020-09-15 00:56:55 +0200

    Consent updates

---
+++ IguideME.Web/wwwroot/precache-manifest.07e2051faaa0b90c7e3f9a8e9c7055c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf7f18d875c8f31044e6e571b7a2325",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5133e1cff57aca394a1a",
+    "url": "/static/css/2.80348574.chunk.css"
+  },
+  {
+    "revision": "6ba502cce7267a02f011",
+    "url": "/static/css/main.bca39324.chunk.css"
+  },
+  {
+    "revision": "5133e1cff57aca394a1a",
+    "url": "/static/js/2.34e25082.chunk.js"
+  },
+  {
+    "revision": "891c960f63d6a1a810bab051132976b7",
+    "url": "/static/js/2.34e25082.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ba502cce7267a02f011",
+    "url": "/static/js/main.1edf6f29.chunk.js"
+  },
+  {
+    "revision": "6ecc21f675d0fc26161f",
+    "url": "/static/js/runtime-main.4d1affbd.js"
+  }
+]);
--- IguideME.Web/wwwroot/service-worker.js
+++ IguideME.Web/wwwroot/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1079a31dd09a5aef166febb35755dd92.js"
+  "/precache-manifest.07e2051faaa0b90c7e3f9a8e9c7055c5.js"
 );
 
 self.addEventListener('message', (event) => {
