9c3c1b4efaef77e0a9258c75c3c5136338cf9b2e
Miguel Pieters <miguelpieters@MBP-van-Miguel.home>
2021-08-09 00:23:15 +0200

    handle consent from component and only enforce checking

--- IguideME.Web/wwwroot/src/hoc/withConsent.tsx
+++ IguideME.Web/wwwroot/src/hoc/withConsent.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ConsentController from "../api/controllers/consent";
 import Loading from "../components/utils/Loading";
+import Consent from "../common/Consent";
+import {store} from "../utils/configureStore";
 
 type IState = {
   consentLoaded: boolean;
   consentGranted: boolean | null;
-
 }
 
 export const withConsent = <P extends object>(Component: React.ComponentType<P>) =>
@@ -26,9 +27,15 @@ export const withConsent = <P extends object>(Component: React.ComponentType<P>)
     }
 
     render() {
-      const { consentLoaded } = this.state;
+      const { consentLoaded, consentGranted } = this.state;
 
-      if (!consentLoaded) return <Loading />;
+      const { course } = store.getState();
+
+      if (!consentLoaded || !course) return <Loading />;
+
+      if (!consentGranted && course.require_consent) {
+        return <Consent text={course.text} />;
+      }
 
       return <Component {...this.props as P} />;
     }
