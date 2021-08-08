25363a2fe4125c3af25cbc257f12efa30cf4532f
Miguel Pieters <miguelpieters@MBP-van-Miguel.home>
2021-08-09 00:21:26 +0200

    create notification serializer

---
+++ IguideME.Web/Models/App/Notification.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace IguideME.Web.Models.App
+{
+    public class Notification
+    {
+        [JsonProperty("tile_id")]
+        public int TileID { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        public Notification(int tileID, string status)
+        {
+            this.TileID = tileID;
+            this.Status = status;
+        }
+    }
+}
