9f1c00fb10f05119e5fbab0e7160af062e507c55
Miguel Pieters <miguelpieters@MBP-van-Miguel.home>
2021-08-09 00:30:06 +0200

    remove

--- IguideME.Web/wwwroot/src/components/upload/UploadBinaryData/StudentsGrid/types.ts
+++
@@ -1,8 +0,0 @@
-import {CanvasStudent} from "../../../../models/canvas/Student";
-
-export interface IProps {
-  students: CanvasStudent[];
-  checked: string[];
-  query: string;
-  setChecked: (checked: string[]) => any;
-}
--- IguideME.Web/wwwroot/src/components/upload/UploadBinaryData/types.ts
+++
@@ -1,3 +0,0 @@
-export interface IProps {
-  closeUploadMenu: () => any;
-}
