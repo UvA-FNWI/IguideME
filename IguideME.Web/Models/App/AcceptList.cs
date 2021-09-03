2e21abc2d7c45dab9a2693e8515989e9c92cefe3
miguelpieters <64592997+miguelpieters@users.noreply.github.com>
2021-09-03 13:54:06 +0200

    handle course accept lists

---
+++ IguideME.Web/Models/App/AcceptList.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace IguideME.Web.Models.App
+{
+    public class AcceptList
+    {
+        [JsonProperty("student_login_id")]
+        public string StudentLoginID { get; set; }
+
+        [JsonProperty("accepted")]
+        public bool Accepted { get; set; }
+
+        public AcceptList(
+            string studentLoginID,
+            bool accepted)
+        {
+            this.StudentLoginID = studentLoginID;
+            this.Accepted = accepted;
+        }
+    }
+}
