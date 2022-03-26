32e64abe03319a024e4bf19afe55cc26bc6d0515
Max Marshall <4356678+everydayanchovies@users.noreply.github.com>
2022-03-26 22:10:13 +0100

    Solved bug due to meta not being provided to all mocks.

--- IguideME.Web/wwwroot/src/components/StudentDashboard/TileDetail/EntriesList/EntryItem/index.tsx
+++ IguideME.Web/wwwroot/src/components/StudentDashboard/TileDetail/EntriesList/EntryItem/index.tsx
@@ -1,47 +1,47 @@
 import React, { Component } from "react";
-import {TileEntry, TileEntrySubmission} from "../../../../../models/app/Tile";
-import {Divider, Statistic, Table} from "antd";
+import { TileEntry, TileEntrySubmission } from "../../../../../models/app/Tile";
+import { Divider, Statistic, Table } from "antd";
 import GradeStatistic from "../../../../../containers/GradeStatistic";
 
 export default class EntryItem extends Component<{
-  submission: TileEntrySubmission,
-  tileEntry: TileEntry
+    submission: TileEntrySubmission,
+    tileEntry: TileEntry
 }> {
-  render(): React.ReactNode {
-    const { tileEntry, submission } = this.props;
+    render(): React.ReactNode {
+        const { tileEntry, submission } = this.props;
 
         // FIXME the check for type is done because when the mocks were made, no consideration was given to the type of meta. To remove this check, convert the meta objects in the mocks to JSON strings.
-        const meta = typeof submission.meta === "string" ? JSON.parse(submission.meta || "{}") : submission.meta;
+        const meta = typeof submission.meta === "string" ? JSON.parse(submission.meta || "{}") : submission.meta || {};
 
-    return (
-      <div className={"tileEntry"}>
+        return (
+            <div className={"tileEntry"}>
                 <h2>{tileEntry.title}</h2>
                 <Divider style={{ margin: '5px 0' }} />
 
-        <GradeStatistic grade={submission.grade} />
+                <GradeStatistic grade={submission.grade} />
 
-        { Object.keys(meta).length > 0 &&
-          <div>
-            <Table dataSource={Object.keys(meta).map((key, i) => ({
-              key: i,
-              label: key,
-              value: meta[key]
-            }))} columns={[
-              {
-                title: 'Key',
-                dataIndex: 'label',
-                key: 'label',
-                width: '40%',
-                ellipsis: true
-              },
-              {
-                title: 'Value',
-                dataIndex: 'value',
-                key: 'value',
-              }]} />
-          </div>
-        }
-      </div>
-    )
-  }
+                { Object.keys(meta).length > 0 &&
+                    <div>
+                        <Table dataSource={Object.keys(meta).map((key, i) => ({
+                            key: i,
+                            label: key,
+                            value: meta[key]
+                        }))} columns={[
+                            {
+                                title: 'Key',
+                                dataIndex: 'label',
+                                key: 'label',
+                                width: '40%',
+                                ellipsis: true
+                            },
+                            {
+                                title: 'Value',
+                                dataIndex: 'value',
+                                key: 'value',
+                            }]} />
+                    </div>
+                }
+            </div>
+        )
+    }
 }
