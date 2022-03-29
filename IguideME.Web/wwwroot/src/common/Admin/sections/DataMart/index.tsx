17366a14be50014cd441ec3fd686ce9dcf658b32
everydayanchovies <4356678+everydayanchovies@users.noreply.github.com>
2022-03-29 21:49:08 +0200

    Fixed console errors regarding map key and DOM.

--- IguideME.Web/wwwroot/src/common/Admin/sections/DataMart/index.tsx
+++ IguideME.Web/wwwroot/src/common/Admin/sections/DataMart/index.tsx
@@ -1,98 +1,98 @@
 import React, { Component } from "react";
 import { Divider } from 'ui-neumorphism'
-import {Spin, Table} from "antd";
+import { Spin, Table } from "antd";
 import Admin from "../../index";
-import {IProps, IState} from "./types";
+import { IProps, IState } from "./types";
 import SyncManager from "../../../../components/managers/SyncManager";
 import DataMartController from "../../../../api/controllers/datamart";
-import {Synchronization} from "../../../../models/app/SyncProvider";
+import { Synchronization } from "../../../../models/app/SyncProvider";
 import moment from "moment";
 import "./style.scss";
 
 export default class DataMart extends Component<IProps, IState> {
 
-  state = {
-    loaded: false,
-    synchronizations: []
-  }
+    state = {
+        loaded: false,
+        synchronizations: []
+    }
 
-  componentDidMount(): void {
-    DataMartController.getSynchronizations().then((synchronizations: Synchronization[]) =>
-      this.setState({
-        // sort synchronizations by their start datetime
-        synchronizations: synchronizations.sort((a, b) =>
-          moment(a.start, 'MMMM Do[,] YYYY [at] LT').isBefore(
-            moment(b.start, 'MMMM Do[,] YYYY [at] LT')
-          ) ? 1 : -1),
-        loaded: true
-      })
-    );
-  }
+    componentDidMount(): void {
+        DataMartController.getSynchronizations().then((synchronizations: Synchronization[]) =>
+            this.setState({
+                // sort synchronizations by their start datetime
+                synchronizations: synchronizations.sort((a, b) =>
+                    moment(a.start, 'MMMM Do[,] YYYY [at] LT').isBefore(
+                        moment(b.start, 'MMMM Do[,] YYYY [at] LT')
+                    ) ? 1 : -1),
+                loaded: true
+            })
+        );
+    }
 
-  render(): React.ReactNode {
-    const { loaded, synchronizations }: IState = this.state;
-    const backendFormat = 'DD/MM/YYYY HH:mm:ss'
-    const timeFormat = 'MMMM Do[,] YYYY [at] LT';
-    const successfulSyncs = loaded ? synchronizations.filter(a => a.status === "COMPLETE") : [];
-    const latestSuccessful = successfulSyncs.length > 0 ? successfulSyncs[0] : null;
+    render(): React.ReactNode {
+        const { loaded, synchronizations }: IState = this.state;
+        const backendFormat = 'DD/MM/YYYY HH:mm:ss'
+        const timeFormat = 'MMMM Do[,] YYYY [at] LT';
+        const successfulSyncs = loaded ? synchronizations.filter(a => a.status === "COMPLETE") : [];
+        const latestSuccessful = successfulSyncs.length > 0 ? successfulSyncs[0] : null;
 
-    return (
-      <Admin menuKey={"datamart"}>
-        <div id={"datamart"}>
-          <h1>Datamart</h1>
-          { loaded ?
-            (latestSuccessful ?
-              <p>
-                The latest successful synchronization took place on
+        return (
+            <Admin menuKey={"datamart"}>
+                <div id={"datamart"}>
+                    <h1>Datamart</h1>
+                    {loaded ?
+                        (latestSuccessful ?
+                            <p>
+                                The latest successful synchronization took place on
                 <b> {moment.utc(latestSuccessful.start, backendFormat).local().format(timeFormat)} </b>
-                <small>({moment.utc(latestSuccessful.start, backendFormat).fromNow()})</small>.
+                                <small>({moment.utc(latestSuccessful.start, backendFormat).fromNow()})</small>.
                 Synchronizations run automatically at 03:00AM (local university time).
               </p> :
-              <p>No historic synchronizations available.</p>) :
-            <p><Spin /> Retrieving latest synchronization...</p>
-          }
+                            <p>No historic synchronizations available.</p>) :
+                        <div><Spin /> Retrieving latest synchronization...</div>
+                    }
 
-          <Divider dense style={{margin: '10px 0'}} />
+                    <Divider dense style={{ margin: '10px 0' }} />
 
-          <SyncManager />
+                    <SyncManager />
 
-          { loaded &&
-            <React.Fragment>
-              <h1 style={{marginTop: 20}}>Historic versions</h1>
-              <Divider dense style={{margin: '10px 0'}} />
+                    {loaded &&
+                        <React.Fragment>
+                            <h1 style={{ marginTop: 20 }}>Historic versions</h1>
+                            <Divider dense style={{ margin: '10px 0' }} />
 
-              <Table scroll={{x: 240}} dataSource={synchronizations} columns={[
-                {
-                  title: 'Start timestamp',
-                  dataIndex: 'start_timestamp',
-                  key: 'start_timestamp',
-                },
-                {
-                  title: 'End timestamp',
-                  dataIndex: 'end_timestamp',
-                  key: 'end_timestamp',
-                },
-                {
-                  title: 'Duration',
-                  dataIndex: 'duration',
-                  key: 'duration',
-                },
-                {
-                  title: 'Hash',
-                  dataIndex: 'hash',
-                  key: 'hash',
-                  render: (val, row) => <code>{val}</code>
-                },
-                {
-                  title: 'Status',
-                  dataIndex: 'status',
-                  key: 'status',
-                },
-              ]} />
-            </React.Fragment>
-          }
-        </div>
-      </Admin>
-    )
-  }
+                            <Table scroll={{ x: 240 }} dataSource={synchronizations} columns={[
+                                {
+                                    title: 'Start timestamp',
+                                    dataIndex: 'start_timestamp',
+                                    key: 'start_timestamp',
+                                },
+                                {
+                                    title: 'End timestamp',
+                                    dataIndex: 'end_timestamp',
+                                    key: 'end_timestamp',
+                                },
+                                {
+                                    title: 'Duration',
+                                    dataIndex: 'duration',
+                                    key: 'duration',
+                                },
+                                {
+                                    title: 'Hash',
+                                    dataIndex: 'hash',
+                                    key: 'hash',
+                                    render: (val, row) => <code>{val}</code>
+                                },
+                                {
+                                    title: 'Status',
+                                    dataIndex: 'status',
+                                    key: 'status',
+                                },
+                            ]} />
+                        </React.Fragment>
+                    }
+                </div>
+            </Admin>
+        )
+    }
 }
--- IguideME.Web/wwwroot/src/components/managers/SyncManager/index.tsx
+++ IguideME.Web/wwwroot/src/components/managers/SyncManager/index.tsx
@@ -1,127 +1,128 @@
 import React, { Component } from "react";
-import {Badge, Col, message, Row} from "antd";
+import { Badge, Col, message, Row } from "antd";
 import SyncOverview from "./SyncOverview";
-import {elapsedTime, syncStates} from "./helpers";
-import {Alert, Caption, Card} from "ui-neumorphism";
-import {IProps, IState} from "./types";
+import { elapsedTime, syncStates } from "./helpers";
+import { Alert, Caption, Card } from "ui-neumorphism";
+import { IProps, IState } from "./types";
 import DataMartController from "../../../api/controllers/datamart";
 import moment from "moment";
 import SyncProvider from "../../../models/app/SyncProvider";
 import Loading from "../../utils/Loading";
-import {debug} from "../../../config/config";
+import { debug } from "../../../config/config";
 
 export default class SyncManager extends Component<IProps, IState> {
-  private interval: NodeJS.Timeout | undefined;
+    private interval: NodeJS.Timeout | undefined;
 
-  state = {
-    loaded: false,
-    start: undefined,
-    elapsedTime: undefined,
-    datamartError: false,
-    currentTask: null,
-    completedTasks: [],
-    currentTasks: []
-  }
+    state = {
+        loaded: false,
+        start: undefined,
+        elapsedTime: undefined,
+        datamartError: false,
+        currentTask: null,
+        completedTasks: [],
+        currentTasks: []
+    }
 
-  componentDidMount(): void {
-    this.setState({ loaded: true });
-  }
+    componentDidMount(): void {
+        this.setState({ loaded: true });
+    }
 
-  componentWillUnmount(): void {
-    if (debug()) localStorage.removeItem("debugHandshake");
-  }
+    componentWillUnmount(): void {
+        if (debug()) localStorage.removeItem("debugHandshake");
+    }
 
-  badgeStyle = (task: string): { color: "success" | "warning" | "error", text: string } => {
-    const { currentTask } = this.state;
-    const idx = syncStates.findIndex(ss => ss.id === task);
-    const currentIdx = syncStates.findIndex(ss => ss.id === currentTask);
+    badgeStyle = (task: string): { color: "success" | "warning" | "error", text: string } => {
+        const { currentTask } = this.state;
+        const idx = syncStates.findIndex(ss => ss.id === task);
+        const currentIdx = syncStates.findIndex(ss => ss.id === currentTask);
 
-    if (idx < currentIdx) {
-      return { color: 'success', text: "Completed"}
-    } else if (idx === currentIdx) {
-      return { color: 'warning', text: "In-progress"}
-    } else return { color: 'error', text: "Unstarted"}
-  }
+        if (idx < currentIdx) {
+            return { color: 'success', text: "Completed" }
+        } else if (idx === currentIdx) {
+            return { color: 'warning', text: "In-progress" }
+        } else return { color: 'error', text: "Unstarted" }
+    }
 
-  startSync = () => {
-    // initialize error prompts
-    this.setState({ datamartError: false });
+    startSync = () => {
+        // initialize error prompts
+        this.setState({ datamartError: false });
 
-    DataMartController.startNewSync().then(success => {
-      if (success) {
-        message.success("Sync started!");
-        this.setState({ start: moment.utc() })
-        this.pollSync();
-      } else this.setState({ datamartError: true });
-    });
-  }
+        DataMartController.startNewSync().then(success => {
+            if (success) {
+                message.success("Sync started!");
+                this.setState({ start: moment.utc() })
+                this.pollSync();
+            } else this.setState({ datamartError: true });
+        });
+    }
 
-  pollSync = () => {
-    // start interval updating the admin's UI every second
-    this.interval = setInterval(async () => {
-      //export const MOCK_DATAMART_STATUS = MOCK_DATAMART_STATUS_BUSY;
-      await DataMartController.getStatus().then(data => {
-        const keys = Object.keys(data);
-        const current = keys.find(k => data[k].progressInformation != SyncProvider.DONE);
+    pollSync = () => {
+        // start interval updating the admin's UI every second
+        this.interval = setInterval(async () => {
+            //export const MOCK_DATAMART_STATUS = MOCK_DATAMART_STATUS_BUSY;
+            await DataMartController.getStatus().then(data => {
+                const keys = Object.keys(data);
+                const current = keys.find(k => data[k].progressInformation != SyncProvider.DONE);
 
-        if (!current) {
-          this.setState({ start: undefined });
-          clearInterval(this.interval!);
-          return;
-        }
+                if (!current) {
+                    this.setState({ start: undefined });
+                    clearInterval(this.interval!);
+                    return;
+                }
 
-        this.setState({
-          elapsedTime: elapsedTime(moment.utc(data[current].startTime)),
-          currentTask: data[current].progressInformation})
-      });
-    }, 1000);
-  }
+                this.setState({
+                    elapsedTime: elapsedTime(moment.utc(data[current].startTime)),
+                    currentTask: data[current].progressInformation
+                })
+            });
+        }, 1000);
+    }
 
-  abortSync = () => {
-    if (this.interval) clearInterval(this.interval);
-    this.setState({ start: undefined, completedTasks: [], currentTasks: [] });
-  }
+    abortSync = () => {
+        if (this.interval) clearInterval(this.interval);
+        this.setState({ start: undefined, completedTasks: [], currentTasks: [] });
+    }
 
-  render(): React.ReactNode {
-    const { loaded, datamartError } = this.state;
+    render(): React.ReactNode {
+        const { loaded, datamartError } = this.state;
 
-    if (!loaded) return <Loading small={true} />;
+        if (!loaded) return <Loading small={true} />;
 
-    return (
-      <Row gutter={[20, 20]}>
-        <Col xs={24} md={12} lg={9}>
-          <SyncOverview startSync={this.startSync}
+        return (
+            <Row gutter={[20, 20]}>
+                <Col xs={24} md={12} lg={9}>
+                    <SyncOverview startSync={this.startSync}
                         abortSync={this.abortSync}
                         elapsed={elapsedTime(this.state.start)} />
 
-          { datamartError &&
-          <Alert type='error' outlined style={{marginTop: 20}}>
-            Failed to reach datamart. Try again later!
+                    {datamartError &&
+                        <Alert type='error' outlined style={{ marginTop: 20 }}>
+                            Failed to reach datamart. Try again later!
           </Alert>
-          }
-        </Col>
-        <Col xs={24} md={12} lg={15}>
-          <Card elevation={1} style={{padding: 10, backgroundColor: 'rgb(246, 248, 250)'}}>
-            <Row gutter={[10, 10]}>
-              { syncStates.map(state => (
-                <Col xs={24} lg={12}>
-                  <Card style={{ display: 'flex', alignItems: 'center'}} elevation={1}>
-                    <Card flat style={{ width: '100%', padding: 20}}>
-                      <Badge status={this.badgeStyle(state.id).color}
-                             text={this.badgeStyle(state.id).text}
-                             style={{float: 'right'}} />
-                      <h4 style={{margin: 0}}>{ state.title }</h4>
-                      <Caption secondary component='span'>
-                        { state.description }
-                      </Caption>
-                    </Card>
-                  </Card>
+                    }
                 </Col>
-              ))}
+                <Col xs={24} md={12} lg={15}>
+                    <Card elevation={1} style={{ padding: 10, backgroundColor: 'rgb(246, 248, 250)' }}>
+                        <Row gutter={[10, 10]}>
+                            {syncStates.map(state => (
+                                <Col key={state.id} xs={24} lg={12}>
+                                    <Card style={{ display: 'flex', alignItems: 'center' }} elevation={1}>
+                                        <Card flat style={{ width: '100%', padding: 20 }}>
+                                            <Badge status={this.badgeStyle(state.id).color}
+                                                text={this.badgeStyle(state.id).text}
+                                                style={{ float: 'right' }} />
+                                            <h4 style={{ margin: 0 }}>{state.title}</h4>
+                                            <Caption secondary component='span'>
+                                                {state.description}
+                                            </Caption>
+                                        </Card>
+                                    </Card>
+                                </Col>
+                            ))}
+                        </Row>
+                    </Card>
+                </Col>
             </Row>
-          </Card>
-        </Col>
-      </Row>
-    )
-  }
+        )
+    }
 }
