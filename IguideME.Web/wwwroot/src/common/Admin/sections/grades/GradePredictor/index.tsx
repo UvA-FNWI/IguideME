2ef3491ce0afa30d02f68aa074beaa8de1ed2299
everydayanchovies <4356678+everydayanchovies@users.noreply.github.com>
2022-06-06 14:04:39 +0200

    Revert "Revert "added new empty grade predictor component""
    This reverts commit 30511d83cce82e85fc186f1e8c47793548bc3ea6.

--- IguideME.Web/wwwroot/src/common/Admin/sections/grades/GradePredictor/index.tsx
+++ IguideME.Web/wwwroot/src/common/Admin/sections/grades/GradePredictor/index.tsx
@@ -1,53 +1,33 @@
 import React, { Component } from "react";
 import Admin from "../../../index";
-import { Divider } from "antd";
-import { IProps, IState } from "./types";
-import ConfigureModel from "./ConfigureModel";
 import Loading from "../../../../../components/utils/Loading";
-import ModelResults from "./ModelResults";
-import { ParentSize } from '@visx/responsive';
-import DataMartController from "../../../../../api/controllers/datamart";
+
+interface IProps {
+
+}
+
+interface IState {
+    loaded: boolean;
+}
 
 export default class GradePredictor extends Component<IProps, IState> {
 
     state = {
         loaded: false,
-        models: [],
-        isConfiguringModel: false
     }
 
     componentDidMount(): void {
-        DataMartController.getModels().then(models => {
-            this.setState({ models, loaded: true });
-        });
+        this.setState({ loaded: true });
     }
 
     render(): React.ReactNode {
-        const { models, loaded, isConfiguringModel } = this.state;
+        const { loaded } = this.state;
 
         if (!loaded)
             return <Loading small={false} />;
 
         return <Admin menuKey={"gradePredictor"}>
             <h1>Grade Predictor</h1>
-            <span onClick={() => this.setState({ isConfiguringModel: true })}>
-                Configure a predictive model
-            </span>
-
-            <Divider />
-
-            {(!isConfiguringModel && models.length > 0) &&
-                <ParentSize>
-                    {parent =>
-                        <ModelResults
-                            models={models}
-                            width={parent.width}
-                            height={500} />}
-                </ParentSize>
-            }
-            {(isConfiguringModel || models.length == 0) &&
-                <ConfigureModel setModels={(models) => this.setState({ models, isConfiguringModel: false })} />
-            }
         </Admin>
     }
 }
