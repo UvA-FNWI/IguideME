30511d83cce82e85fc186f1e8c47793548bc3ea6
everydayanchovies <4356678+everydayanchovies@users.noreply.github.com>
2022-06-06 14:02:43 +0200

    Revert "added new empty grade predictor component"
    This reverts commit 72df8f498d2bbacbbc367f88d27a4f24f20d0c12.

--- IguideME.Web/wwwroot/src/app.tsx
+++ IguideME.Web/wwwroot/src/app.tsx
@@ -2,14 +2,14 @@ import React, { Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Loading from "./components/utils/Loading";
 import DesiredGrade from "./common/DesiredGrade";
-import { RootState } from "./store";
-import { TileActions } from "./store/actions/tiles";
-import { connect, ConnectedProps } from "react-redux";
-import { ThunkDispatch } from "redux-thunk";
-import { UserActions } from "./store/actions/user";
-import { CourseActions } from "./store/actions/course";
-import { DataMartActions } from "./store/actions/datamart";
-import { AppActions } from "./store/actions/app";
+import {RootState} from "./store";
+import {TileActions} from "./store/actions/tiles";
+import {connect, ConnectedProps} from "react-redux";
+import {ThunkDispatch} from "redux-thunk";
+import {UserActions} from "./store/actions/user";
+import {CourseActions} from "./store/actions/course";
+import {DataMartActions} from "./store/actions/datamart";
+import {AppActions} from "./store/actions/app";
 
 const App = React.lazy(() => import('./common/App'));
 const Consent = React.lazy(() => import('./common/Consent'));
@@ -21,29 +21,28 @@ const AdminStudentOverview = React.lazy(() => import('./common/Admin/sections/St
 const AdminDataWizard = React.lazy(() => import('./common/Admin/sections/DataWizard'));
 const AdminGradeAnalyzer = React.lazy(() => import('./common/Admin/sections/grades/GradeAnalyzer'));
 const AdminGradePredictor = React.lazy(() => import('./common/Admin/sections/grades/GradePredictor'));
-const AdminGradePredictorOld = React.lazy(() => import('./common/Admin/sections/grades/GradePredictorOld'));
 const AdminSettings = React.lazy(() => import('./common/Admin/sections/Settings'));
 
 const mapState = (state: RootState) => ({
-    dashboardColumns: state.dashboardColumns,
-    tiles: state.tiles,
-    tileEntries: state.tileEntries,
-    tileGroups: state.tileGroups,
-    course: state.course,
-    user: state.user,
-    predictions: state.predictions
+  dashboardColumns: state.dashboardColumns,
+  tiles: state.tiles,
+  tileEntries: state.tileEntries,
+  tileGroups: state.tileGroups,
+  course: state.course,
+  user: state.user,
+  predictions: state.predictions
 });
 
-const mapDispatch = (dispatch: ThunkDispatch<any, any, any>): any => {
-    return {
-        loadTiles: async () => dispatch(await TileActions.loadTiles()),
-        loadTileGroups: async () => dispatch(await TileActions.loadGroups()),
-        loadTileEntries: async () => dispatch(await TileActions.loadTileEntries()),
-        loadCourse: async () => dispatch(await CourseActions.loadCourse()),
-        getUser: async () => dispatch(await UserActions.getUser()),
-        loadPredictions: async () => dispatch(await DataMartActions.loadPredictions()),
-        loadColumns: async () => dispatch(await AppActions.loadColumns()),
-    };
+const mapDispatch = ( dispatch: ThunkDispatch<any, any, any> ): any => {
+  return {
+    loadTiles: async () => dispatch(await TileActions.loadTiles()),
+    loadTileGroups: async () => dispatch(await TileActions.loadGroups()),
+    loadTileEntries: async () => dispatch(await TileActions.loadTileEntries()),
+    loadCourse: async () => dispatch(await CourseActions.loadCourse()),
+    getUser: async () => dispatch(await UserActions.getUser()),
+    loadPredictions: async () => dispatch(await DataMartActions.loadPredictions()),
+    loadColumns: async () => dispatch(await AppActions.loadColumns()),
+  };
 };
 
 const connector = connect(mapState, mapDispatch)
@@ -53,47 +52,46 @@ type Props = PropsFromRedux;
 
 class IGuideME extends React.Component<Props> {
 
-    componentDidMount(): void {
-        const { tiles, tileEntries, tileGroups, user, course, predictions, dashboardColumns }: Props = this.props;
+  componentDidMount(): void {
+    const { tiles, tileEntries, tileGroups, user, course, predictions, dashboardColumns }: Props = this.props;
 
-        if (tiles.length === 0) this.props.loadTiles();
-        if (tileGroups.length === 0) this.props.loadTileGroups();
-        if (tileEntries.length === 0) this.props.loadTileEntries();
-        if (dashboardColumns.length === 0) this.props.loadColumns();
-        if (predictions.length === 0) this.props.loadPredictions();
-        if (!user) this.props.getUser();
-        if (!course) this.props.loadCourse();
-    }
+    if (tiles.length === 0) this.props.loadTiles();
+    if (tileGroups.length === 0) this.props.loadTileGroups();
+    if (tileEntries.length === 0) this.props.loadTileEntries();
+    if (dashboardColumns.length === 0) this.props.loadColumns();
+    if (predictions.length === 0) this.props.loadPredictions();
+    if (!user) this.props.getUser();
+    if (!course) this.props.loadCourse();
+  }
 
-    render(): React.ReactNode {
-        const { user, course } = this.props;
-        if (!user || !course) return <Loading />;
+  render(): React.ReactNode {
+    const { user, course } = this.props;
+    if (!user || !course) return <Loading />;
 
-        return (
-            <div>
-                <Suspense fallback={<Loading />}>
-                    <Router>
-                        { /* debug() && <DebugMenu /> */}
-                        <Switch>
-                            <Route path="/admin/analytics" component={AdminAnalytics} />
-                            <Route path="/admin/dashboard" component={AdminDashboard} />
-                            <Route path="/admin/student-overview" component={AdminStudentOverview} />
-                            <Route path="/admin/data-wizard" component={AdminDataWizard} />
-                            <Route path="/admin/grade-analyzer" component={AdminGradeAnalyzer} />
-                            <Route path="/admin/grade-predictor-old" component={AdminGradePredictorOld} />
-                            <Route path="/admin/grade-predictor" component={AdminGradePredictor} />
-                            <Route path="/admin/settings" component={AdminSettings} />
-                            <Route path="/admin/tiles" component={AdminTiles} />
-                            <Route path="/admin" component={AdminDataMart} />
-                            <Route path="/goal-grade" component={DesiredGrade} />
-                            <Route path="/consent" component={Consent} />
-                            <Route path="/" component={App} />
-                        </Switch>
-                    </Router>
-                </Suspense>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <Suspense fallback={<Loading />}>
+          <Router>
+            { /* debug() && <DebugMenu /> */ }
+            <Switch>
+              <Route path="/admin/analytics" component={AdminAnalytics} />
+              <Route path="/admin/dashboard" component={AdminDashboard} />
+              <Route path="/admin/student-overview" component={AdminStudentOverview} />
+              <Route path="/admin/data-wizard" component={AdminDataWizard} />
+              <Route path="/admin/grade-analyzer" component={AdminGradeAnalyzer} />
+              <Route path="/admin/grade-predictor" component={AdminGradePredictor} />
+              <Route path="/admin/settings" component={AdminSettings} />
+              <Route path="/admin/tiles" component={AdminTiles} />
+              <Route path="/admin" component={AdminDataMart} />
+              <Route path="/goal-grade" component={DesiredGrade} />
+              <Route path="/consent" component={Consent} />
+              <Route path="/" component={App} />
+            </Switch>
+          </Router>
+        </Suspense>
+      </div>
+    );
+  }
 }
 
 export default connector(IGuideME);
--- IguideME.Web/wwwroot/src/common/Admin/sections/grades/GradePredictor/index.tsx
+++ IguideME.Web/wwwroot/src/common/Admin/sections/grades/GradePredictor/index.tsx
@@ -1,33 +1,53 @@
 import React, { Component } from "react";
 import Admin from "../../../index";
+import { Divider } from "antd";
+import { IProps, IState } from "./types";
+import ConfigureModel from "./ConfigureModel";
 import Loading from "../../../../../components/utils/Loading";
-
-interface IProps {
-
-}
-
-interface IState {
-    loaded: boolean;
-}
+import ModelResults from "./ModelResults";
+import { ParentSize } from '@visx/responsive';
+import DataMartController from "../../../../../api/controllers/datamart";
 
 export default class GradePredictor extends Component<IProps, IState> {
 
     state = {
         loaded: false,
+        models: [],
+        isConfiguringModel: false
     }
 
     componentDidMount(): void {
-        this.setState({ loaded: true });
+        DataMartController.getModels().then(models => {
+            this.setState({ models, loaded: true });
+        });
     }
 
     render(): React.ReactNode {
-        const { loaded } = this.state;
+        const { models, loaded, isConfiguringModel } = this.state;
 
         if (!loaded)
             return <Loading small={false} />;
 
         return <Admin menuKey={"gradePredictor"}>
             <h1>Grade Predictor</h1>
+            <span onClick={() => this.setState({ isConfiguringModel: true })}>
+                Configure a predictive model
+            </span>
+
+            <Divider />
+
+            {(!isConfiguringModel && models.length > 0) &&
+                <ParentSize>
+                    {parent =>
+                        <ModelResults
+                            models={models}
+                            width={parent.width}
+                            height={500} />}
+                </ParentSize>
+            }
+            {(isConfiguringModel || models.length == 0) &&
+                <ConfigureModel setModels={(models) => this.setState({ models, isConfiguringModel: false })} />
+            }
         </Admin>
     }
 }
