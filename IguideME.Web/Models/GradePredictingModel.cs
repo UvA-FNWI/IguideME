7b138bb0ce04b006ee7d95bd82bdc3c4d2437a3c
everydayanchovies <4356678+everydayanchovies@users.noreply.github.com>
2022-07-22 15:50:23 +0200

    broken wip

---
+++ IguideME.Web/Models/GradePredictingModel.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace IguideME.Web.Models.App
+{
+    public class GradePredictionModel
+    {
+        [JsonProperty("id")]
+        public int ID { get; set; }
+
+        [JsonProperty("course_id")]
+        public int CourseID { get; set; }
+
+        public GradePredictionModel(
+            int id,
+            int courseID)
+        {
+            this.ID = id;
+            this.CourseID = courseID;
+        }
+    }
+}
