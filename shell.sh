37dac66758bd442fb2482e55b0aa31d6a7905ec4
everydayanchovies <4356678+everydayanchovies@users.noreply.github.com>
2022-07-22 15:59:42 +0200

    nix

--- shell.sh
+++ shell.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-nix-shell
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # dotnet not currently available for aarch64 M1 chips on nix
+    nix-shell --pure --option system x86_64-darwin
+else
+    nix-shell --pure
+fi
