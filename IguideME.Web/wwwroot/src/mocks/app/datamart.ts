82d65a22ecd83ebd548cf06c3c21c8f3391f2109
Anthony Kirkinis <akirkinis@gmail.com>
2023-04-22 23:50:24 +0200

    historic data

--- IguideME.Web/wwwroot/src/mocks/app/datamart.ts
+++ IguideME.Web/wwwroot/src/mocks/app/datamart.ts
@@ -5,7 +5,7 @@ export const MOCK_DATAMART_SYNCHRONIZATIONS = [
     key: '1',
     start_timestamp: `${moment.utc().subtract(1, 'days').format('MM/DD/YY [03:00:20]')}`,
     end_timestamp: `${moment.utc().subtract(1, 'days').format('MM/DD/YY [03:06:02]')}`,
-    hash: "KVC8UCSAYE",
+    hash: `${moment.utc().subtract(1, 'days').format('MM/DD/YY [03:00:20]')}`,
     duration: "349",
     invoked: 'IGUIDEME SYSTEM',
     status: "COMPLETE"
@@ -14,7 +14,7 @@ export const MOCK_DATAMART_SYNCHRONIZATIONS = [
     key: '2',
     start_timestamp: `${moment.utc().format('MM/DD/YY [03:00:20]')}`,
     end_timestamp: `${moment.utc().format('MM/DD/YY [03:06:02]')}`,
-    hash: "VQNAXCCISY",
+    hash: `${moment.utc().format('MM/DD/YY [03:00:20]')}`,
     duration: "372",
     invoked: 'IGUIDEME SYSTEM',
     status: "COMPLETE"
@@ -23,7 +23,7 @@ export const MOCK_DATAMART_SYNCHRONIZATIONS = [
     key: '3',
     start_timestamp: `${moment.utc().subtract(3, 'hours').format('MM/DD/YY [10:27:15]')}`,
     end_timestamp: `${moment.utc().subtract(3, 'hours').add(5, 'minutes').format('MM/DD/YY [10:32:20]')}`,
-    hash: "HGKZFP4QT6",
+    hash: `${moment.utc().subtract(3, 'hours').format('MM/DD/YY [10:27:15]')}`,
     duration: "291",
     invoked: 'Demo Account',
     status: "Aborted"
