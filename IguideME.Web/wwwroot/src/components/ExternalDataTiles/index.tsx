b13bb4af839ab3c46a44ea09504057314d399317
Steven Lageveen <stevenmax888@gmail.com>
2023-05-29 14:32:09 +0200

    Added rudimentary sorting to data wizard

--- IguideME.Web/wwwroot/src/components/ExternalDataTiles/index.tsx
+++ IguideME.Web/wwwroot/src/components/ExternalDataTiles/index.tsx
@@ -7,7 +7,7 @@ export default class ExternalDataTiles extends Component<IProps> {
     const { tiles, tileGroups } = this.props;
     return (
       <div>
-        { tiles.map((t, i) => (
+        { tiles.sort((a, b) => a.group_id * 100 + a.id - b.group_id * 100 - b.id).map((t, i) => (
           <div key={i}>
             <ExternalTile tile={t}
                           tileGroup={tileGroups.find(g => g.id === t.group_id)!} />
