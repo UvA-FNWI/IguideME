195635de0c692c7a0a60e52257f01ea4677476d7
TijnvdK <tijn.van.den.kommer@student.uva.nl>
2024-04-06 16:19:20 +0200

    chore: Add all files to the tailwind config and not just the files in the components folder.

--- IguideME.Web/Frontend/tailwind.config.ts
+++ IguideME.Web/Frontend/tailwind.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   darkMode: ['class'],
   theme: {
     extend: {
