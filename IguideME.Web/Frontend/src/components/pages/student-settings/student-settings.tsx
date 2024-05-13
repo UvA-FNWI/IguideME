153070d1e519adcd46acaad10600d3bd723d1845
ThijsJ04 <tijnvdk@gmail.com>
2024-05-13 10:15:04 +0200

    style: make the consent form collapsible.

--- IguideME.Web/Frontend/src/components/pages/student-settings/student-settings.tsx
+++ IguideME.Web/Frontend/src/components/pages/student-settings/student-settings.tsx
@@ -2,7 +2,7 @@ import { postConsentSettings, postGoalGrade, postNotificationSettings } from '@/
 import { getSelf } from '@/api/users';
 import Loading from '@/components/particles/loading';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { Checkbox, Divider, Radio, Space, Switch } from 'antd';
+import { Checkbox, Collapse, Divider, Radio, Space, Switch } from 'antd';
 import { FC, ReactElement, useEffect } from 'react';
 import { toast } from 'sonner';
 
@@ -99,23 +99,12 @@ const GoalGrade: FC<GoalProps> = ({ goalGrade }): ReactElement => {
       <p className='text-justify text-base'>
         Please indicate the grade you wish to obtain for this course. You can always change your goal at a later stage!
       </p>
-      <div className='w-full'>
+      <div className='mt-2 w-full'>
         <Space className='mx-auto' direction={'vertical'}>
           <Radio.Group
             value={goalGrade}
             onChange={(val) => saveGoalGrade(val.target.value)}
-            options={[
-              { label: '1', value: 1 },
-              { label: '2', value: 2 },
-              { label: '3', value: 3 },
-              { label: '4', value: 4 },
-              { label: '5', value: 5 },
-              { label: '6', value: 6 },
-              { label: '7', value: 7 },
-              { label: '8', value: 8 },
-              { label: '9', value: 9 },
-              { label: '10', value: 10 },
-            ]}
+            options={Array.from({ length: 10 }, (_, i) => ({ label: `${i + 1}`, value: i + 1 }))}
             optionType='button'
           />
         </Space>
@@ -143,78 +132,97 @@ const Consent: FC<ConsentProps> = ({ consent }): ReactElement => {
       });
     }
   }, [status]);
+
   return (
     <>
       <h2 className='text-left text-2xl'>Informed Consent</h2>
-      <p className='text-justify text-base'>
-        Please read the informed consent carefully. You will be asked to accept the informed consent, if declined your
-        data will not be processed. You can change your preference at any time.
-      </p>
+      <Collapse
+        bordered={false}
+        className='[&>div>div]:!p-0'
+        ghost
+        items={[
+          {
+            key: 1,
+            label: (
+              <>
+                <p className='text-justify text-base'>
+                  Please read the informed consent carefully. You will be asked to accept the informed consent, if
+                  declined your data will not be processed. You can change your preference at any time.
+                  <span className='sr-only'>Click anywhere on this text to open the informed consent.</span>
+                </p>
+              </>
+            ),
+            children: <ConsentText />,
+            showArrow: true,
+          },
+        ]}
+      />
 
-      <div className='bg-primary-gray prose m-6 rounded-lg p-2 text-justify prose-h1:text-text prose-h2:text-text prose-h3:text-text'>
-        <h1>IguideME</h1>
-        <h2>INFORMED CONSENT</h2>
-        <p>
-          Dear participant,
-          <br />
-          We ask for your cooperation in an evaluation study into educational improvement. In this document, the
-          so-called "informed consent", we explain this study and you can indicate whether you want to cooperate. Read
-          the text below carefully.
-        </p>
-        <h3>Goal of the research</h3>
-        <p>
-          The goal of this educational research is to study the effects of the feedback tool “IguideME” and activating
-          learning tools (e.g. Perusall) on the learning process.
-          <br />
-          The results of this research can be used to facilitate your learning process, to improve the design of this
-          and other courses, and for scientific publications.
-        </p>
-        <h3>Research description</h3>
-        <p>
-          To investigate the effects of using IguideME, personal data (name and student ID) and learning activity data
-          will be collected. Based on these data, you will receive personal feedback via the IguideME dashboard in
-          Canvas. You will also receive a predicted course grade that will be automatically calculated from the learning
-          activity data using an algorithm. To investigate the effects of activating learning tools, the quality of
-          assignments will be assessed and the results of a short questionnaire that scores motivation and learning
-          behavior will be compared between the beginning and at the end of the course. For presentations purposes, all
-          data will be anonymized.
-        </p>
-        <h3>Voluntariness</h3>
-        <p>
-          The participation in this research is voluntary. In the case that you decline to participate or stop your
-          participation the data that you have generated will not be used in the research. You are free to stop your
-          participation in this research without specifying a reason by informing dr. Erwin van Vliet.
-        </p>
-        <h3>Insurance</h3>
-        <p>
-          This research brings no risks for your health and safety and in this case the regular liability insurance of
-          the University of Amsterdam is valid.
-        </p>
-        <h3>Additional Information</h3>
-        <p>
-          In case of any questions about this research please contact: <b>TEACHER NAME</b> (<b>ROLE</b>), phone{' '}
-          <b>PHONE-NUMBER</b>, e-mail <b>EMAIL@DOMAIN.XYZ</b>
-        </p>
-        <h2>CONSENT FORM</h2>
-        <p>Here you will be asked to sign the Informed consent.</p>
-        <ul>
-          <li className='text-text'>
-            By choosing <i>"Yes"</i> in this form, you declare that you have read the document entitled “informed
-            consent IguideME”, understood it, and confirm that you agree with the procedure as described.
-            <br />
-          </li>
-          <li className='text-text'>
-            By choosing <i>"No"</i> in the form, you declare that you have read the document entitled “informed consent
-            IguideME”, understood it, and confirm that you do not want to participate in this study.
-          </li>
-        </ul>
-      </div>
-
-      <Checkbox checked={consent} onChange={(e) => saveConsent(e.target.checked)}>
+      <Checkbox className='mt-4' checked={consent} onChange={(e) => saveConsent(e.target.checked)}>
         <p>I have read and understood the informed consent</p>
       </Checkbox>
     </>
   );
 };
+
+const ConsentText: FC = () => (
+  <div className='bg-primary-gray prose rounded-lg p-2 text-justify prose-h1:text-text prose-h2:text-text prose-h3:text-text'>
+    <h1>IguideME</h1>
+    <h2>INFORMED CONSENT</h2>
+    <p>
+      Dear participant,
+      <br />
+      We ask for your cooperation in an evaluation study into educational improvement. In this document, the so-called
+      "informed consent", we explain this study and you can indicate whether you want to cooperate. Read the text below
+      carefully.
+    </p>
+    <h3>Goal of the research</h3>
+    <p>
+      The goal of this educational research is to study the effects of the feedback tool “IguideME” and activating
+      learning tools (e.g. Perusall) on the learning process.
+      <br />
+      The results of this research can be used to facilitate your learning process, to improve the design of this and
+      other courses, and for scientific publications.
+    </p>
+    <h3>Research description</h3>
+    <p>
+      To investigate the effects of using IguideME, personal data (name and student ID) and learning activity data will
+      be collected. Based on these data, you will receive personal feedback via the IguideME dashboard in Canvas. You
+      will also receive a predicted course grade that will be automatically calculated from the learning activity data
+      using an algorithm. To investigate the effects of activating learning tools, the quality of assignments will be
+      assessed and the results of a short questionnaire that scores motivation and learning behavior will be compared
+      between the beginning and at the end of the course. For presentations purposes, all data will be anonymized.
+    </p>
+    <h3>Voluntariness</h3>
+    <p>
+      The participation in this research is voluntary. In the case that you decline to participate or stop your
+      participation the data that you have generated will not be used in the research. You are free to stop your
+      participation in this research without specifying a reason by informing dr. Erwin van Vliet.
+    </p>
+    <h3>Insurance</h3>
+    <p>
+      This research brings no risks for your health and safety and in this case the regular liability insurance of the
+      University of Amsterdam is valid.
+    </p>
+    <h3>Additional Information</h3>
+    <p>
+      In case of any questions about this research please contact: <b>TEACHER NAME</b> (<b>ROLE</b>), phone{' '}
+      <b>PHONE-NUMBER</b>, e-mail <b>EMAIL@DOMAIN.XYZ</b>
+    </p>
+    <h2>CONSENT FORM</h2>
+    <p>Here you will be asked to sign the Informed consent.</p>
+    <ul>
+      <li className='text-text'>
+        By choosing <i>"Yes"</i> in this form, you declare that you have read the document entitled “informed consent
+        IguideME”, understood it, and confirm that you agree with the procedure as described.
+        <br />
+      </li>
+      <li className='text-text'>
+        By choosing <i>"No"</i> in the form, you declare that you have read the document entitled “informed consent
+        IguideME”, understood it, and confirm that you do not want to participate in this study.
+      </li>
+    </ul>
+  </div>
+);
 
 export default StudentSettings;
